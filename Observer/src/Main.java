ce404ce0f666d14e5e9b8aa4d55c8906814dea47
adnanavni <adnan.avni@hotmail.com>
2023-02-06 20:46:44 +0200

    observer

---
+++ Observer/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ClockTimer timer = new ClockTimer(0, 0, 0);
+		DigitalClock clock = new DigitalClock(timer);
+		timer.run();
+	}
+
+}
